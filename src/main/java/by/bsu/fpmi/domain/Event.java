8537e2bf35675329e955239159a96603607af58b
apostalik <zmicervadim@gmail.com>
2013-05-01 12:57:34 +0300

    add folders, some models, dependency for mapping

---
+++ src/main/java/by/bsu/fpmi/domain/Event.java
@@ -0,0 +1,10 @@
+package by.bsu.fpmi.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "EVENTS")
+public class Event {
+
+}
---
+++ src/main/java/by/bsu/fpmi/domain/Profile.java
@@ -0,0 +1,5 @@
+package by.bsu.fpmi.domain;
+
+public class Profile {
+
+}
